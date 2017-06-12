c7e86f0e8dec386d4548225ae7d44ac33d227336
mintyacer <gituser55@gmail.com>
2017-06-11 21:14:48 -0400

    changes to server

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var express = require("express"),
-    app = express();
+    app = express(),
+    mongoClient = require('mongodb').MongoClient
+    foodinfoService = require("./services/foodinfoService");
 
 app.use(express.static("/home/mintyacer/Documents/webProjects/foodCalculator/resources"));
+
+app.get("services/foodinfo", foodInfoService(mongoConncection));
 
 app.use(function (req, res, next) {
   res.status(404).send("404 FILE NOT FOUND");
